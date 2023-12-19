5605aad01cb9a40d8827d47e7004ea4288d757b5
Daniel Schulte <daniel.schulte@webfleet.com>
2023-12-19 14:23:37 +0100

    * switch to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+
+plugins {
+    kotlin("jvm") version "1.9.21"
+    application
+}
+
+group = "c64"
+version = "1.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass.set("c64.emulation.ui.EmulatorUIKt")
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("io.github.microutils:kotlin-logging:3.0.5")
+
+    implementation("org.slf4j:slf4j-api:2.0.3")
+    implementation("org.slf4j:slf4j-log4j12:2.0.3")
+}
